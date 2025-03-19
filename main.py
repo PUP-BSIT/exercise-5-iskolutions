2acb1161c88ca48bc074775a5e20b3f5cb6a7c80
franco-fernettepearl <fernettepearl.franco55@gmail.com>
2025-03-19 15:26:52 +0800

    Complete Questions 3 and 4

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# ToDo(Efondo, Aaron Kyle): Do question 1 and 2
+
 print("QUIZ")
 score = 0
 
+# Question 1
 print("")
 print("1. (Aaron Kyle D. Efondo) What is the primary function of a router?")
 print("a) Connectivity \tb) Storage")
@@ -13,6 +14,7 @@ if answer == "A":
 else:
     print(f"{answer} is incorrect. The correct answer is A")
 
+# Question 2
 print("")
 print("2. (Aaron Kyle D. Efondo) What does 'HTTP' stand for?")
 print("a) HyperText Transfer Protocol \t\tb) High Text Transfer Protocol")
@@ -24,7 +26,29 @@ if answer == "A":
 else:
     print(f"{answer} is incorrect. The correct answer is A")
 
-# ToDO(Franco, Fernette Pearl): Do question 3 and 4
+# Question 3
+print("")
+print("3. (Fernette Pearl Franco) Who wrote the play of Romeo and Juliet?")
+print("a) Jane Austen        \tb) J.K. Rowling")
+print("c) George R.R. Martin \td) William Shakespeare")
+answer = input("Enter your answer: ").upper()
+if answer == "D":
+    print(f"{answer} is correct!")
+    score += 1
+else:
+    print(f"{answer} is incorrect. The correct answer is D")
+
+# Question 4
+print("")
+print("4. (Fernette Pearl Franco) The largest planet in the solar system?")
+print("a) Earth \tb) Jupiter")
+print("c) Mars \td) Saturn")
+answer = input("Enter your answer: ").upper()
+if answer == "B":
+    print(f"{answer} is correct!")
+    score += 1
+else:
+    print(f"{answer} is incorrect. The correct answer is B")
 
 # ToDO(Lopez, Jakim): Do question 5 and 6
 
