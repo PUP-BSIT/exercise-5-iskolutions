5724cb9815ca5b9ee0786b31854cbbe42e7d05d6
efondo_aaronkyle <akyleefondo@gmail.com>
2025-03-19 15:01:24 +0800

    add quiz items

---
+++ main.py
@@ -0,0 +1,36 @@
+# ToDo(Efondo, Aaron Kyle): Do question 1 and 2
+print("QUIZ")
+score = 0
+
+print("")
+print("1. (Aaron Kyle D. Efondo) What is the primary function of a router?")
+print("a) Connectivity \tb) Storage")
+print("c) Processing \t\td) Output") 
+answer = input("Enter your answer: ").upper()
+if answer == "A":
+    print(f"{answer} is correct!")
+    score += 1
+else:
+    print(f"{answer} is incorrect. The correct answer is A")
+
+print("")
+print("2. (Aaron Kyle D. Efondo) What does 'HTTP' stand for?")
+print("a) HyperText Transfer Protocol \t\tb) High Text Transfer Protocol")
+print("c) Hyper Tech Transmission Protocol \td) High Transfer Text Protocol") 
+answer = input("Enter your answer: ").upper()
+if answer == "A":
+    print(f"{answer} is correct!")
+    score += 1
+else:
+    print(f"{answer} is incorrect. The correct answer is A")
+
+# ToDO(Franco, Fernette Pearl): Do question 3 and 4
+
+# ToDO(Lopez, Jakim): Do question 5 and 6
+
+# ToDO(Gagtan, Hanz Matthew): Do question 7 and 8
+
+# ToDO(Causon, Miko Lorenz): Do question 9 and 10
+
+print(f"Congratulations! You got {score} out of 10 items!")
+
